5a8d43a5fae81c3827b907e236bc489992f8b222
Sunhapuk Nilsai <your-email@example.com>
2026-02-10 15:54:32 +0100

    Added backend project files

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    // await mongoose.connect("mongodb+srv://mdhadmin:gTVfymZ8nAMft630@clustermdh.1zcryka.mongodb.net/?appName=ClusterMDH");
+    await mongoose.connect("mongodb+srv://vamanreddy87658:CU70x3n38Nyf7VC4@backenddb.x8tvvh1.mongodb.net/profileDB?retryWrites=true&w=majority")
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
