378da9e71dfbaea5b8edee6a77e6263c2bcda2e4
Hemanth Malagalale Marishetty <hemanthmm@stud.mdh.de>
2026-02-11 14:27:12 +0100

    removed hardcode ID

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:5000/api";
 
-//  HARDCODE USER ID HERE( For testing)
-const USER_ID = "65d4f1e2b1c2d3e4f5a67890";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
