93fb0bbaa7c6c5f501772a7058ba001def6ac172
Sunhapuk Nilsai <your-email@example.com>
2026-02-11 13:12:49 +0100

    I chang port to 3000 because i use Macbook and i delete 3 comments from my code.

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
