76786995840a838c713aa3aea0a5a6e9d02bb112
Sunhapuk Nilsai <your-email@example.com>
2026-02-11 13:35:46 +0100

    good

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,6 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
