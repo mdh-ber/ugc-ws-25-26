dcc38b8af3e4b91213b0126720a4487b5e043135
Sunhapuk Nilsai <sunilsai@stud.mediadesign.de>
2026-02-11 12:19:21 +0000

    change port

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,6 @@ app.use("/api/trainings", require("./routes/trainingRoutes"));
 const profileRoutes = require("./routes/profileRoutes");
 app.use("/api/profiles", profileRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
