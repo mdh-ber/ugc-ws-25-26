38a85bb476e6f5ceff015fbc8f3e933d58ea6de6
Vivekrajsinh Mayurdhvajsinh Vadajiya <vimayurdhvajsinh@stud.mediadesign.de>
2026-02-14 03:02:45 +0100

    everything okey

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
+app.use('/api/events', eventRoutes);
 
 app.use("/api/review-requests", require("./routes/reviewRequestRoutes"));
 
