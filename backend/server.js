4ed123d0ce45cfa106dfaa81f48bc54d1e8ef8ca
Sunhapuk Nilsai <your-email@example.com>
2026-02-18 14:47:37 +0100

    create chart for leads with filterable backend and database

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use("/api/review-requests", require("./routes/reviewRequestRoutes"));
 app.use("/api/trainings", require("./routes/trainingRoutes"));
 app.use("/api/profiles", require("./routes/profileRoutes"));
 app.use("/api/uu", require("./routes/uuRoutes"));
-app.use("/api/events", require("./routes/eventRoutes"));
+app.use("/api/lead", require("./routes/leadRoutes"));
 
 // ⭐ YOUR GUIDELINES ROUTE
 app.use("/api/guidelines", require("./routes/guidelinesRoutes"));
@@ -39,7 +39,7 @@ app.use("/api/guidelines", require("./routes/guidelinesRoutes"));
 // =====================
 // SERVER START
 // =====================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
