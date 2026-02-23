cc74ed55de95f36f0985efd271994e1f74ca9cdf
BHARADWAZ CIRASANAGANDLA <cibharadwaz@stud.mediadesign.de>
2026-02-23 11:49:47 +0100

    fix: resolve routes conflict

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,25 +5,18 @@ import Rewards from "./pages/Rewards";
 import Milestones from "./pages/Milestones";
 import CreatorPerformanceDashboard from "./pages/CreatorPerformanceDashboard";
 
-// import other pages that already exist in main...
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// etc...
-
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* existing routes from main */}
       <Route path="/rewards" element={<Rewards />} />
       <Route path="/milestones" element={<Milestones />} />
 
-      {/* your new route */}
       <Route
         path="/mdh/creator-performance"
         element={<CreatorPerformanceDashboard />}
       />
 
-      {/* keep other routes that were already here */}
+      {/* keep other routes that were already here (add them below) */}
     </Routes>
   );
 }
