d37f231e570c8df53fd6f19bf410421fc0f9759b
BHARADWAZ CIRASANAGANDLA <cibharadwaz@stud.mediadesign.de>
2026-02-23 11:46:38 +0100

    fix: resolve routes merge conflict

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,14 +1,29 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Rewards from "./pages/Rewards";
-
-<Route path="/rewards" element={<Rewards />} />
-
 import Milestones from "./pages/Milestones";
-
-<Route path="/milestones" element={<Milestones />}/>
-
 import CreatorPerformanceDashboard from "./pages/CreatorPerformanceDashboard";
 
-<Route
-  path="/mdh/creator-performance"
-  element={<CreatorPerformanceDashboard />}
-/>
+// import other pages that already exist in main...
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// etc...
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      {/* existing routes from main */}
+      <Route path="/rewards" element={<Rewards />} />
+      <Route path="/milestones" element={<Milestones />} />
+
+      {/* your new route */}
+      <Route
+        path="/mdh/creator-performance"
+        element={<CreatorPerformanceDashboard />}
+      />
+
+      {/* keep other routes that were already here */}
+    </Routes>
+  );
+}
